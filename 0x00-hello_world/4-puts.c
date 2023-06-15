2c1154c1af579d9f6c9555d4dbc5c910a9eccd74
group12343 <onegroup935@gmail.com>
2023-06-15 15:47:30 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /**
- * Return:0
- * my_function - This is a description
- */
-int main(void){
+*main  my_function - This is a description
+*return:0
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
