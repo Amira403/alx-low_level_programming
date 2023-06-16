02d1d1256fb44f4e7cb453149fbb0faaf89363ff
group12343 <onegroup935@gmail.com>
2023-06-16 15:31:21 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c  = 97 ; c < 123 ; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n);
+	return (0);
+}
