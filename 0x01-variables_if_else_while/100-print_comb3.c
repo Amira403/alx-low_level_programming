888930fcf6be53c7181c2b813ab7e96a977f5df4
group12343 <onegroup935@gmail.com>
2023-06-18 14:46:11 -0700

    l

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 /**
- *my function
- *Rreturn 0; 
+ * main - my function
+ *Rreturn: 0 
  */
-int main() {
+int main(void)
+{
 	int d1,d2;
 
-	for (d1=0; d1 <10 ;d1++){
-	for (d2=d1 +1; d2 <10;d2++)
+	for (d1 = 0 ; d1 < 10 ; d1++)
 	{
-	putchar(d1 +'0');
+	for (d2 = d1 +1 ; d2 < 10 ; 2++)
+	{
+	putchar(d1 + '0');
 	putchar(d2 + '0');
 	if (d1 == 8 && d2 == 9){
-	return 0;
+	return (0);
 	}
 	putchar(',');
 	putchar(' ');
 	}
 	}
-	return 0;
+	return (0);
 }
