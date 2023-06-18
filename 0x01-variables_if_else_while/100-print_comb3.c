9d491ad828ea65ad2f025b23a43c714a59f4d04f
group12343 <onegroup935@gmail.com>
2023-06-18 14:48:46 -0700

    l

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,18 @@ int main(void)
 
 	for (d1 = 0 ; d1 < 10 ; d1++)
 	{
-	for (d2 = d1 +1 ; d2 < 10 ; 2++)
+	for (d2 = d1 + 1 ; d2 < 10 ; d2++)
 	{
 	putchar(d1 + '0');
 	putchar(d2 + '0');
-	if (d1 == 8 && d2 == 9){
+	if (d1 == 8 && d2 == 9)
+	{
 	return (0);
 	}
 	putchar(',');
 	putchar(' ');
 	}
+	putchar('\n');
 	}
 	return (0);
 }
