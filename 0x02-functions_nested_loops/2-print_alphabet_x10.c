d3880649d775ea1f9af358aac0fc2bb386225309
group12343 <onegroup935@gmail.com>
2023-06-20 15:17:24 -0700

    k

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+        int x, c;
+
+        for (x = 0 ; x < 10 ; x++)
+        {
+        for (c = 97 ; c < 123 ; c++)
+        {
+        _putchar(c);
+        }
+        _putchar('\n');
+}
