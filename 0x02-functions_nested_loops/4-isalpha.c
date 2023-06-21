66a604f19aca10a26b07ea4f2e3d8b0d17fa6541
group12343 <onegroup935@gmail.com>
2023-06-21 15:43:46 -0700

    k

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,12 @@
  */
 int _isalpha(int c)
 {
-	if ( c >= 'a' ||  c <='z' || c >= 'A' ||  c <='Z' )
+	if (c >= 'a' &&  c <= 'z' || c >= 'A' &&  c <= 'Z')
 	{
 	return (1);
 	}
-	else 
+	else
 	{
 	return (0);
-	}    
+	}
 }
