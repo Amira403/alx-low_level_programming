1a1e12c8b7d9493a18688dad1949730bf91c593e
group12343 <onegroup935@gmail.com>
2023-06-22 16:55:55 -0700

    k

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,20 +10,3 @@ int mul(int a, int b)
 {
 	return (a * b);
 }
-
-
-2-main.c
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	return (0);
-}
