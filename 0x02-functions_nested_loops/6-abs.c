8e4d2df93e531809cca5c4e5863a29a763a3b586
group12343 <onegroup935@gmail.com>
2023-06-21 17:03:35 -0700

    k

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * _abs - computes the absolute value of an integer
- * @n: the int to check
+ * @s: the int to check
  * Return: the absolute value of int
  */
 int _abs(int s)
