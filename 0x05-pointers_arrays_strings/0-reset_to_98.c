8349004b91f2a8a7e32ab60c641e193b981198fc
group12343 <onegroup935@gmail.com>
2023-06-26 15:51:43 -0700

    n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function that takes a pointer to an int as paramet * er and upd * ates the value it points to to 98
+ * @*n:pointer 
+ * @p:int
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	int p = 42;
+	int *n = &p;
+
+	updateto98(n);
+	putchar('9');
+	putchar('8');
+	return (0);
+}
