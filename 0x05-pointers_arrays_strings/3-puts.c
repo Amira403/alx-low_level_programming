6814039dd70b3b170f7fc0d7a54abf252bd54c48
group12343 <onegroup935@gmail.com>
2023-06-29 01:42:40 -0700

    f

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts -  prints a string
+ * @str: the str to print
+ * Return: void 
+ */
+void _puts(char *str)
+{       
+	for (; str != '\0';str++)
+	{       
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
