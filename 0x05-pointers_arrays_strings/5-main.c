70469ceec58c23625561b02babf2b89f2815db8c
group12343 <onegroup935@gmail.com>
2023-06-29 02:51:44 -0700

    f

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[10] = "My School";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
