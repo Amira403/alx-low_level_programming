7f650a73cc9a4a26a23e6398405d34f03a71d27f
group12343 <onegroup935@gmail.com>
2023-06-29 13:36:08 -0700

    f

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string 
+ * @dest:destantion value 
+ * @scr: source value 
+ * Return:  the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; scr[i] != '0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	dest[1++] = '\0';
+	return (dest);
+}
+
