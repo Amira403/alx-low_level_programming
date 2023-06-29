645714fa2792e038aef7a09fda17f14578588a8b
group12343 <onegroup935@gmail.com>
2023-06-29 02:53:00 -0700

    f

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * rev_string - reverses a string.
- * @s: the str to be modified 
+ * @s: the str to be modified
  * Return: void
  */
 void rev_string(char *s)
 {
-	int len =0, index = 0;
+	int len = 0, index = 0;
 	char m;
 
 	while (s[index++])
