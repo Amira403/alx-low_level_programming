456e3448d7fe53a2065f07cb25103ce7096aeb9c
group12343 <onegroup935@gmail.com>
2023-06-30 14:07:34 +0000

    f

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/
  * rev_string - reverse array
  * @n: integer params
  * Return: 0
@@ -17,7 +17,6 @@ void rev_string(char *n)
 	i++;
 	}
 	i--;
-
 	for (j = 0; j < i; j++, i--)
 	{
 	temp = *(n + j);
@@ -25,8 +24,7 @@ void rev_string(char *n)
 	*(n + i) = temp;
 	}
 	}
-
-/**
+/
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
