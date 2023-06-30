7c58a0a164fd9fc62c268109fe5883fa19a40523
group12343 <onegroup935@gmail.com>
2023-06-30 12:59:48 +0000

    f

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,9 @@
+
 #include "main.h"
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
+ *
  * Return: n
  */
 char *string_toupper(char *n)
@@ -14,6 +16,6 @@ char *string_toupper(char *n)
 	if (n[i] >= 'a' && n[i] <= 'z')
 	n[i] = n[i] - 32;
 	i++;
-	}		
+	}
 	return (n);
 }
