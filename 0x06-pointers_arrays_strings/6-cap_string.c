34ea8ab5b4e89388a4c2838ab53f7017991261ee
group12343 <onegroup935@gmail.com>
2023-06-30 00:09:24 +0000

    f

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -8,10 +7,10 @@
  */
 int main(void)
 {
-	char str[] = "Look up!\n";
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 	char *ptr;
 
-	ptr = string_toupper(str);
+	ptr = cap_string(str);
 	printf("%s", ptr);
 	printf("%s", str);
 	return (0);
