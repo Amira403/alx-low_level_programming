25ce59979ed9a3ff9c197382419b5fe1fbda8f2a
group12343 <onegroup935@gmail.com>
2023-06-30 13:05:09 +0000

    f

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,19 +13,19 @@ char *cap_string(char *str)
 	{
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
-	if (str[index - 1] == ' '||
-	str[index - 1] == '\t'|| 
-	str[index - 1] == '\n'|| 
-	str[index - 1] == ','|| 
-	str[index - 1] == ';'|| 
-	str[index - 1] == '.'|| 
-	str[index - 1] == '!'|| 
-	str[index - 1] == '?'|| 
-	str[index - 1] == '"'||
-	str[index - 1] == '('|| 
-	str[index - 1] == ')'||
-	str[index - 1] == '{'|| 
-	str[index - 1] == '}'||
+	if (str[index - 1] == ' ' ||
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
+	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
+	str[index - 1] == '(' ||
+	str[index - 1] == ')' ||
+	str[index - 1] == '{' ||
+	str[index - 1] == '}' ||
 	index == 0)
 	str[index] -= 32;
 	index++;
