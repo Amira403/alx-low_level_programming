f5437d7cc0c0a59f2b69a310a6de0ca60915a132
group12343 <onegroup935@gmail.com>
2023-07-19 09:45:09 -0700

    f

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,35 +4,32 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 	i++;
 	}
 	return (i);
 }
-
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
- *
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
-
 	len = 0;
+
 	while (src[len] != '\0')
 	{
 	len++;
@@ -44,13 +41,11 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
-
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
