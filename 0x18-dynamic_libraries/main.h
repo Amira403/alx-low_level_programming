0844506be60da06bdb4788b9599ec59046e231c4
group12343 <onegroup935@gmail.com>
2023-09-23 13:13:17 -0700

    mo

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DYNLIB
+#define DYNLIB
 
-#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -25,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
