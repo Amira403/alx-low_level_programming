64921a90ec165563559a6887b3e6b7e17ad4bd52
group12343 <onegroup935@gmail.com>
2023-09-23 13:27:43 -0700

    mo

--- 0x18-dynamic_libraries/README.md
+++
@@ -1,1 +0,0 @@
-ALX SE Dynamic libraries
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,7 @@
-#ifndef DYNLIB
-#define DYNLIB
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -24,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
